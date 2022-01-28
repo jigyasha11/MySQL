d4e56d12dbfa1ad1a36b30336d7404eaef28a70b
Jigyasha Baruah <91016552+jigyasha11@users.noreply.github.com>
2022-01-28 21:26:12 +0530

    Table Employee
    DBMS practical example 1

---
+++ example1.sql
@@ -0,0 +1,17 @@
+create table Employee (
+Emp_no int (5) primary key,
+Emp_name varchar (20),
+Join_date date,
+Basic_pay_fixed_at int(7),
+Date_of_birth date
+);
+
+
+insert into values
+(1001,"charles Babbage",'2000-06-01',8000,'1973-10-03'),
+(1002,"George Boole",'2001-06-01',5000,'1972-12-04'),
+(1003,"E.f codd",'2001-06-01',8000,'1969-03-06'),
+(1004,"Bill Gates",'2003-07-01',5000,'1995-10-09'),
+(1005,"Tony Greig",'2004-08-01',8000,'1985-05-04');
+
+select * from Employee;
